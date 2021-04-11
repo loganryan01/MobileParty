68970393c9ce11d9ca24a337a8aa97552599bd44
Logan Ryan <loganryan982@gmail.com>
2021-04-11 11:21:06 +1000

    Added dice block prefab

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -25,7 +25,21 @@ public class PlayerScript : MonoBehaviour
         // Check if the player is touching the screen
         if (Input.touchCount > 0)
         {
-            Debug.Log(Input.touchCount);
+            // Get the touch position
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = touch.position;
+
+            // Check if the player has touched the dice block
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.CompareTag("DiceBlock"))
+                {
+                    Debug.Log("Touching Dice Block");
+                }
+            }
         }
 
         // Mouse controls are to be deleted when releasing a build
@@ -33,11 +47,26 @@ public class PlayerScript : MonoBehaviour
         // Check if the player is touching the screen
         if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
         {
-            Debug.Log("2");
+
         }
         else if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
-            Debug.Log("1");
+            // Get the touch position
+            Vector2 touchPosition = Input.mousePosition;
+
+            // Check if the player has touched the dice block
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.CompareTag("DiceBlock"))
+                {
+                    Debug.Log("Touching Dice Block");
+
+                    // TODO - Make player jump when they touch the dice block
+                }
+            }
         }
     }
 }
